7d66f00e701265df2989bbc0d12b822d2c3e5180
Samuel W <68044712+Samuel-KW@users.noreply.github.com>
2023-09-13 09:58:23 -0700

    Adding mathjax

--- src/inject/index.js
+++ src/inject/index.js
@@ -1,8 +1,51 @@
 class QuizletLatex {
 	constructor () {
-		
+		this.observeHomepage();
 	}
+
+	observeHomepage () {
+		const carousel = document.querySelector('.MiniFlashcards');
+		const terms = document.querySelector('section.SetPageTerms-termsList');
+
+		this.watchAndLatex(carousel);
+	}
+
+	latex (element) {
+		MathJax.Hub.Queue(["Typeset", MathJax.Hub,math]);
+	}
+
+	watchAndLatex (query) {
+		this.observe(query, mutations => {
+			console.log(mutations);
+		});
+	}
+
+	/**
+	 * Handle element mutations
+	 * @param {string} query HTML element to observe
+	 * @param {Function} callback Callback function
+	 */
+	observe (element, callback) {
+
+		// Create listener
+		const observer = new MutationObserver(function () {
+			callback.apply(this, arguments);
+		});
+
+		const config = {
+			childList: true, // direct children
+			subtree: true 	 // lower descendants too
+		};
+		
+		// Start observing
+		return observer.observe(element, config);
+	}
+
 }
 
-const Qz = new QuizletLatex();
-window.Qz = Qz;
+
+window.addEventListener('DOMContentLoaded', () => {
+
+	const Qz = new QuizletLatex();
+	window.Qz = Qz;
+});
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,6 +1,15 @@
 
-// Inject script
-const script = document.createElement('script');
-script.src = chrome.runtime.getURL('src/inject/index.js');
-script.onload = () => script.remove();
-(document.head || document.documentElement).appendChild(script);
+// Inject MathJax
+const MathJax = document.createElement('script');
+MathJax.src = chrome.runtime.getURL('src/inject/mathjax.js');
+MathJax.onload = () => {
+
+	const script = document.createElement('script');
+	script.src = chrome.runtime.getURL('src/inject/index.js');
+	script.onload = () => script.remove();
+	(document.head || document.documentElement).appendChild(script);
+
+	MathJax.remove();
+}
+(document.head || document.documentElement).appendChild(MathJax);
+
